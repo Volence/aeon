ac0c0ce0efe4b8de79228f918d364ce249669f88
Volence <steve@volence.dev>
2026-06-23 00:09:31 -0400

    style(traversal): align edge_mode reserved-pad comment column in OJZ descriptor

--- data/levels/ojz/act1/act_descriptor.asm
+++ data/levels/ojz/act1/act_descriptor.asm
@@ -38,7 +38,7 @@ OJZ_Act1_Descriptor:
     dc.l    OJZ_Act_Pool_PageTable  ; act_art_pool_table (Act Art Streaming Phase 1)
     dc.w    OJZ_ACT_POOL_PAGES      ; act_art_pool_pages
     dc.b    EDGE_CLAMP              ; edge_mode (Phase 2 §10; OJZ ships CLAMP)
-    dc.b    0                      ; reserved (pad)
+    dc.b    0                       ; reserved (pad)
     align 2
 
 ; -----------------------------------------------
